b63cd3f0c956c5e2145d434f3c8d0240ad3e0988
AkmatovDastanbek <124769056+AkmatovDastanbek@users.noreply.github.com>
2023-03-08 21:46:43 +0600

    full

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,25 @@
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        ArrayList<Integer> numbers = new ArrayList<>();
+        for (int i = 1; i <= 100; i++) {
+            numbers.add(i);
+        }
+
+        ArrayList<Integer> oddNumbers = new ArrayList<>();
+        ArrayList<Integer> evenNumbers = new ArrayList<>();
+
+        for (int num : numbers) {
+            if (num % 2 == 0) {
+                evenNumbers.add(num);
+            } else {
+                oddNumbers.add(num);
+            }
+        }
+
+        System.out.println("Нечетные цифры: " + oddNumbers);
+        System.out.println("Четные цифры: " + evenNumbers);
     }
+
 }
